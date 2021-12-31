3e893826681a50fb728906d3ea5511ca2392737a
choesechan <seachan94@naver.com>
2022-01-01 00:21:26 +0900

    fix : stateFlow 사용시 코드 추가 ( 주석처리 )

--- app/src/main/java/com/example/testyogiyo/ui/MainActivity.kt
+++ app/src/main/java/com/example/testyogiyo/ui/MainActivity.kt
@@ -10,6 +10,7 @@ import com.example.testyogiyo.R
 import com.example.testyogiyo.databinding.ActivityMainBinding
 import com.example.testyogiyo.ui.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -45,6 +46,21 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+        //stateFlow를 사용하면 다음과 같이 사용할 수 있지만 clean 아키텍쳐를 적용하지 않는다면 굳이..?
+
+//        lifecycleScope.launchWhenStarted {
+//            viewModel.searchText.collect {
+//                if(viewModel.fragmentLayout.value == viewModel.listOfFragment[0]){
+//                    viewModel.requestUser()
+//                }
+//                else {
+//                    lifecycleScope.launch {
+//                        viewModel.getUserFromDB()
+//                    }
+//                }
+//            }
+//        }
 
     }
 
